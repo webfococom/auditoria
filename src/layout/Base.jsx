60d7dd50a3d1d2199a3ba4437c5e93a30df66b67
alexjunior012 <alexjunior.web@gmail.com>
2019-10-21 09:13:21 -0300

    Comentando scripts de js

--- src/layout/Base.jsx
+++ src/layout/Base.jsx
@@ -13,25 +13,27 @@ class Base extends React.Component {
         /**
          * Variables
          */
-        const signupButton = document.getElementById('signup-button'),
+        /*const signupButton = document.getElementById('signup-button'),
             loginButton = document.getElementById('login-button'),
-            userForms = document.getElementById('user_options-forms');
+            userForms = document.getElementById('user_options-forms');*/
 
         /**
          * Add event listener to the "Sign Up" button
          */
-        signupButton.addEventListener('click', () => {
+        /*signupButton.addEventListener('click', () => {
             userForms.classList.remove('bounceRight');
             userForms.classList.add('bounceLeft');
-        }, false);
+        }, false);*/
 
         /**
          * Add event listener to the "Login" button
          */
+        /*
         loginButton.addEventListener('click', () => {
             userForms.classList.remove('bounceLeft');
             userForms.classList.add('bounceRight');
         }, false)
+        */
     }
 
     render() {
