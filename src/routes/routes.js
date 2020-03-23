d183bafa4eea51bbedc63f5d1864a32146c96a2b
alexjunior012 <alexjunior.web@gmail.com>
2020-03-23 13:02:03 -0300

    tela de locais

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,6 +10,7 @@ import Bot from "views/bot/Bot";
 import Login from "views/campanhas/PainelDaCampanha";
 
 import GMBIndex from 'views/googleMeuNegocio';
+import GMBLocais from 'views/googleMeuNegocio/locais';
 import GMBPostagens from 'views/googleMeuNegocio/postagens';
 import GMBInformacoes from 'views/googleMeuNegocio/informacoes';
 import GMBResultados from 'views/googleMeuNegocio/resultados';
@@ -92,6 +93,12 @@ let routes = [
         path: "/google-meu-negocio",
         exact: true,
         component: GMBIndex,
+    },
+    {
+        name: "Locais",
+        path: "/google-meu-negocio/locais",
+        exact: true,
+        component: GMBLocais,
     },
     {
         name: "Postagens",
