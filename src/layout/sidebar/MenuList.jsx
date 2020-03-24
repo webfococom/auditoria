6cfdb3d7414105b83b22d886d27798020cf5b4d1
Wallace Rio <wallrio@gmail.com>
2020-03-24 17:37:30 -0300

    ajuste no menu principal

---
+++ src/layout/sidebar/MenuList.jsx
@@ -0,0 +1,190 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faRobot, faBriefcase,faAd,faFileWord,faStore} from '@fortawesome/free-solid-svg-icons';
+
+
+import "./menuStyle.scss";
+import { Tabbordion, TabPanel, TabLabel, TabContent } from 'react-tabbordion'
+
+
+class MenuList extends React.Component {
+    render() {
+
+    	const blockElements = {
+            content: 'tabs-content',
+            panel: 'tabs-panel',
+            label: 'tabs-title'
+        }
+
+        return (
+            <Tabbordion blockElements={blockElements} mode="toggle" className="accordion" name="accordion" initialPanels={0}>
+                 {/* GOOGLE MEU NEGOCIO */}
+                <TabPanel>
+                    <TabLabel><FontAwesomeIcon icon={faBriefcase} /> Google Meu Negócio</TabLabel>
+                    <TabContent>
+                       
+                        <ul className="nav nav-pills nav-sidebar flex-column"
+                            data-widget="treeview-menu"
+                            role="menu"
+                            data-accordion="false">
+                            <li className="nav-item">
+                                <Link to="/google-meu-negocio" className="nav-link">
+                                    <p>Início</p>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/google-meu-negocio/locais" className="nav-link">
+                                    <p>locais</p>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/google-meu-negocio/postagens" className="nav-link">
+                                    <p>Postagens</p>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/google-meu-negocio/informacoes" className="nav-link">
+                                    <p>Informações</p>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/google-meu-negocio/resultados" className="nav-link">
+                                    <p>Resultados</p>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/google-meu-negocio/avaliacoes" className="nav-link">
+                                    <p>Avaliações</p>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/google-meu-negocio/mensagens" className="nav-link">
+                                    <p>Mensagens</p>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/google-meu-negocio/fotos" className="nav-link">
+                                    <p>Fotos</p>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/google-meu-negocio/produtos" className="nav-link">
+                                    <p>Produtos</p>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/google-meu-negocio/servicos" className="nav-link">
+                                    <p>Serviços</p>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/google-meu-negocio/configuracoes" className="nav-link">
+                                    <p>Configurações</p>
+                                </Link>
+                            </li>
+                        </ul>
+                    </TabContent>
+                </TabPanel>
+
+                {/* GOOGLE ADS */}
+                <TabPanel>
+                    <TabLabel><FontAwesomeIcon icon={faAd} /> Google Ads</TabLabel>
+                    <TabContent>
+                       
+                        <ul className="nav nav-pills nav-sidebar flex-column"
+                            data-widget="treeview-menu"
+                            role="menu"
+                            data-accordion="false"
+                        >
+                            <li className="nav-item">
+                                <Link to="/google-ads" className="nav-link">
+                                    <p>Visão geral</p>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/google-ads/oportunidades" className="nav-link">
+                                    <p>Oportunidades</p>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/google-ads/piloto-automatico" className="nav-link">
+                                    <p>Piloto Automático</p>
+                                </Link>
+                            </li>
+                        </ul>
+                    </TabContent>
+                </TabPanel>
+
+                {/* PRODUTOS */}
+                <TabPanel>
+                    <TabLabel><FontAwesomeIcon icon={faStore} /> Produtos</TabLabel>
+                    <TabContent>
+                        <ul className="nav nav-pills nav-sidebar flex-column"
+                            data-widget="treeview-menu"
+                            role="menu"
+                            data-accordion="false"
+                        >
+                        <li className="nav-item">
+                            <Link to="/produtos" className="nav-link">
+                                <p>Listagem</p>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/produtos/new" className="nav-link">
+                                <p>Cadastrar Produtos</p>
+                            </Link>
+                        </li>
+                    </ul>
+                    </TabContent>
+                </TabPanel>
+
+                {/* LANDING PAGES */}
+                <TabPanel>
+                    <TabLabel><FontAwesomeIcon icon={faFileWord} /> Landing Pages</TabLabel>
+                    <TabContent>
+                      
+                        <ul className="nav nav-pills nav-sidebar flex-column"
+                            data-widget="treeview-menu"
+                            role="menu"
+                            data-accordion="false"
+                        >
+                            <li className="nav-item">
+                                <Link to="/landing-pages" className="nav-link">
+                                    <p>Dashboard</p>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/landing-pages/new" className="nav-link">
+                                    <p>Novas landing page</p>
+                                </Link>
+                            </li>
+                        </ul>
+                    </TabContent>
+                </TabPanel>
+
+                {/* CHATBOT */}
+                <TabPanel>
+                    <TabLabel><FontAwesomeIcon icon={faRobot} /> Chatbot</TabLabel>
+                    <TabContent>
+                      
+                     
+                    <ul className="nav nav-pills nav-sidebar flex-column"
+                        data-widget="treeview-menu"
+                        role="menu"
+                        data-accordion="false"
+                    >
+                        <li className="nav-item">
+                            <Link to="/bot" className="nav-link">
+                                <p>Cadastro</p>
+                            </Link>
+                        </li>
+                    </ul>
+                    </TabContent>
+                </TabPanel>
+            </Tabbordion>
+        );
+    }
+}
+
+export default MenuList;
--- src/layout/sidebar/Sidebar.jsx
+++ src/layout/sidebar/Sidebar.jsx
@@ -11,6 +11,10 @@ import {
     faMapMarkedAlt
 } from '@fortawesome/free-solid-svg-icons';
 
+import MenuList from "layout/sidebar/MenuList";
+
+
+
 class Sidebar extends React.Component {
     constructor(props) {
         super(props);
@@ -29,231 +33,18 @@ class Sidebar extends React.Component {
     }
 
     render() {
+
         return (
             <aside className="main-sidebar sidebar-collapse sidebar-ads">
-                {/*sidebar-mini layout-fixed sidebar-collapse*/}
-                {/*sidebar-mini layout-fixed sidebar-open*/}
                 <Link className="brand-link toggle-menu" onClick={this.openCloseMenu}>
-                    <Link to="/" style={{width: '100%'}}>
-                        <img src={require('assets/images/logo-cielo.png')} style={{width: '100px', justifyItems:'center'}}/>
+                    <Link to="/" style={{width: '100%',display:'inline-block',textAlign:'center'}}>
+                        <img src={require('assets/images/logo-cielo-ads.png')} style={{width: '130px', justifyItems:'center'}}/>
                     </Link>
-                    {/* <FontAwesomeIcon icon={faBars} /> */}
-                    {/* <span className="name">
-                        Ads-Hub
-                    </span> */}
                 </Link>
-
-                {/*<a href="index3.html" className="brand-link">
-                    <img src="dist/img/AdminLTELogo.png"
-                         alt="AUDITORIA ADS"
-                         className="brand-image img-circle elevation-3"
-                         style={{"opacity": ".8"}} />
-                    <span className="brand-text font-weight-light">AUDITORIA ADS</span>
-                </a>*/}
 
                 <div className="sidebar">
                     <nav className="mt-2">
-                        <ul className="nav nav-pills nav-sidebar flex-column"
-                            data-widget="treeview-menu"
-                            role="menu"
-                            data-accordion="false">
-                                
-                            {/* GOOGLE MEU NEGOCIO */}
-                            <li className="nav-item">
-                                {/* <Link to="/" className="nav-link">
-                                    <FontAwesomeIcon icon={faMapMarkedAlt} /> 
-                                    <p>Google Meu Negócio</p>
-                                </Link> */}
-                                <p>Google Meu Negócio</p>
-                                <ul className="nav nav-pills nav-sidebar flex-column"
-                                    data-widget="treeview-menu"
-                                    role="menu"
-                                    data-accordion="false">
-                                    <li className="nav-item">
-                                        <Link to="/google-meu-negocio" className="nav-link">
-                                            <p>Início</p>
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link to="/google-meu-negocio/locais" className="nav-link">
-                                            <p>locais</p>
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link to="/google-meu-negocio/postagens" className="nav-link">
-                                            <p>Postagens</p>
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link to="/google-meu-negocio/informacoes" className="nav-link">
-                                            <p>Informações</p>
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link to="/google-meu-negocio/resultados" className="nav-link">
-                                            <p>Resultados</p>
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link to="/google-meu-negocio/avaliacoes" className="nav-link">
-                                            <p>Avaliações</p>
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link to="/google-meu-negocio/mensagens" className="nav-link">
-                                            <p>Mensagens</p>
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link to="/google-meu-negocio/fotos" className="nav-link">
-                                            <p>Fotos</p>
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link to="/google-meu-negocio/produtos" className="nav-link">
-                                            <p>Produtos</p>
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link to="/google-meu-negocio/servicos" className="nav-link">
-                                            <p>Serviços</p>
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link to="/google-meu-negocio/configuracoes" className="nav-link">
-                                            <p>Configurações</p>
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </li>
-                            {/* GOOGLE MEU NEGOCIO */}
-
-                            {/* GOOGLE ADS */}
-                            <li className="nav-item">
-                                {/* <div>
-                                    <FontAwesomeIcon icon={faHome} /> <p>Google Ads</p>
-                                </div> */}
-                                <p>Google Ads</p>
-                                <ul className="nav nav-pills nav-sidebar flex-column"
-                                    data-widget="treeview-menu"
-                                    role="menu"
-                                    data-accordion="false"
-                                >
-                                    <li className="nav-item">
-                                        <Link to="/google-ads" className="nav-link">
-                                            <p>Visão geral</p>
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link to="/google-ads/oportunidades" className="nav-link">
-                                            <p>Oportunidades</p>
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link to="/google-ads/piloto-automatico" className="nav-link">
-                                            <p>Piloto Automático</p>
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </li>
-                            {/* GOOGLE ADS */}
-
-                            {/* PRODUTOS */}
-                            <li className="nav-item">                           
-                                <p>Produtos</p>
-                                <ul className="nav nav-pills nav-sidebar flex-column"
-                                    data-widget="treeview-menu"
-                                    role="menu"
-                                    data-accordion="false"
-                                >
-                                    <li className="nav-item">
-                                        <Link to="/produtos" className="nav-link">
-                                            <p>Listagem</p>
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link to="/produtos/new" className="nav-link">
-                                            <p>Cadastrar Produtos</p>
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </li>
-
-                            {/* LANDING PAGES */}
-                             <li className="nav-item">
-                                {/* <Link to="/" className="nav-link">
-                                    <FontAwesomeIcon icon={faHome} /> <p>Landing Pages</p>
-                                </Link> */}
-                                <p>Landing Pages</p>
-                                <ul className="nav nav-pills nav-sidebar flex-column"
-                                    data-widget="treeview-menu"
-                                    role="menu"
-                                    data-accordion="false"
-                                >
-                                    <li className="nav-item">
-                                        <Link to="/landing-pages" className="nav-link">
-                                            <p>Dashboard</p>
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link to="/landing-pages/new" className="nav-link">
-                                            <p>Novas landing page</p>
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </li>
-
-                            {/* CHATBOT */}
-                            <li className="nav-item">
-                                <p><FontAwesomeIcon icon={faRobot} /> Chatbot</p>
-                                
-                                <ul className="nav nav-pills nav-sidebar flex-column"
-                                    data-widget="treeview-menu"
-                                    role="menu"
-                                    data-accordion="false"
-                                >
-                                    <li className="nav-item">
-                                        <Link to="/bot" className="nav-link">
-                                            <p>Cadastro</p>
-                                        </Link>
-                                    </li>
-                                </ul>
-
-                            </li>
-                            {/* LANDING PAGES */}
-
-
-                            {/* <li className="nav-item">
-                                <Link to="/" className="nav-link">
-                                    <FontAwesomeIcon icon={faHome} /> <p>Home</p>
-                                </Link>
-                            </li>
-
-                            <li className="nav-item">
-                                <Link to="/anuncios" className="nav-link">
-                                    <FontAwesomeIcon icon={faBullhorn} /> <p>Anúncios</p>
-                                </Link>
-                            </li>
-
-                            <li className="nav-item">
-                                <Link to="/raio-x" className="nav-link">
-                                    <FontAwesomeIcon icon={faHeartbeat} /> <p>Raio X</p>
-                                </Link>
-                            </li>
-
-                            <li className="nav-item">
-                                <Link to="/mixer" className="nav-link">
-                                    <FontAwesomeIcon icon={faHome} /> <p>Mixer</p>
-                                </Link>
-                            </li>
-
-                            <li className="nav-item">
-                                <Link to="/resultados" className="nav-link">
-                                    <FontAwesomeIcon icon={faChartLine} /> <p>Resultados</p>
-                                </Link>
-                            </li>
-                        </li> */}
-                        </ul>
+                        <MenuList/>
                     </nav>
                 </div>
             </aside>
