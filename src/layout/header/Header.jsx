27ddfc2d61a373ad29e2049f0a3b5472e9d87e0d
alexjunior012 <alexjunior.web@gmail.com>
2020-03-25 15:31:44 -0300

    alterações no topo e na tela de cadastro

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBell } from '@fortawesome/free-solid-svg-icons';
 
@@ -7,55 +6,39 @@ class Header extends React.Component {
     render() {
         return (
             <nav className="main-header navbar navbar-expand navbar-ads">
-                <ul className="navbar-nav">
-                    <Link to="/">
-                        {/* <img src={require('assets/images/auditoria.png')} /> */}
-                    </Link>
-                </ul>
+                <div class="dropdown drop-cielo">
+                    <button class="btn btn-primary btn-primary-cielo dropdown-toggle" type="button" id="btn-empresas-cielo" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Cielo Ads
+                    </button>
+
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="#">Google</a>
+                        <a class="dropdown-item" href="#">Webfoco</a>
+                    </div>
+                </div>
 
                 <ul className="navbar-nav ml-auto">
                     <li className="nav-item dropdown">
                         <a className="nav-link" data-toggle="dropdown" href="#">
-                            <FontAwesomeIcon icon={faBell} />
-                            <span className="badge badge-warning navbar-badge">15</span>
+                            <FontAwesomeIcon icon={faBell} color="#fff" />
+                            <span className="badge badge-warning navbar-badge">2</span>
                         </a>
                         <div className="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                            <span className="dropdown-item dropdown-header">15 Notifications</span>
+                            <span className="dropdown-item dropdown-header">2 Notificações</span>
                             <div className="dropdown-divider"></div>
                             <a href="#" className="dropdown-item">
-                                <i className="fas fa-envelope mr-2"></i> 4 new messages
-                                <span className="float-right text-muted text-sm">3 mins</span>
+                                <i className="fas fa-envelope mr-2"></i> 1 novo local adicionado
+                                <span className="float-right text-muted text-sm">3 min</span>
                             </a>
                             <div className="dropdown-divider"></div>
                             <a href="#" className="dropdown-item">
-                                <i className="fas fa-users mr-2"></i> 8 friend requests
-                                <span className="float-right text-muted text-sm">12 hours</span>
+                                <i className="fas fa-file mr-2"></i> 2 novos sites
+                                <span className="float-right text-muted text-sm">2 dias</span>
                             </a>
                             <div className="dropdown-divider"></div>
-                            <a href="#" className="dropdown-item">
-                                <i className="fas fa-file mr-2"></i> 3 new reports
-                                <span className="float-right text-muted text-sm">2 days</span>
-                            </a>
-                            <div className="dropdown-divider"></div>
-                            <a href="#" className="dropdown-item dropdown-footer">See All Notifications</a>
+                            <a href="#" className="dropdown-item dropdown-footer">Ver todas notificações</a>
                         </div>
                     </li>
-
-                    {/* <li className="nav-item dropdown">
-                        <div className="user-panel mt-3 pb-3 mb-3 d-flex">
-                            <div className="image">
-                                <img src={require('assets/images/user.jpg')}
-                                     className="img-circle elevation-2"
-                                     alt="José da silva"/>
-                            </div>
-                            <div className="info">
-                                <a href="#" className="d-block">José da silva</a>
-                            </div>
-                        </div>
-                    </li> */}                   
-
-
-
                 </ul>
             </nav>
         );
